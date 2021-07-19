033c62c71a4335d3d7fbdaff07fc9d855418eba3
Brianna Wang <74729817+briannawang@users.noreply.github.com>
2021-07-19 14:40:32 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ColourBox from './components/colourSheet.js';
+
+function App() {
+  return (
+    <div className="App">
+      <ColourBox />
+    </div>
+  );
+}
+
+export default App;
